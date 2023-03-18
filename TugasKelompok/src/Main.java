c5468b15ae97ac6288da2ed9214c43015ccf84f6
Alief Zulvan <depanb187@gmail.com>
2023-03-18 21:22:59 +0700

    adkbaldavdad

--- TugasKelompok/src/Main.java
+++ TugasKelompok/src/Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Main.main()");
 //        adhakldaldb
+//    adkadb dkad adkavd
 //        cek
 //cek cek
     }
