150bc8cc9fe6464fa96b05f215b25f0eb8391e4d
Alief Zulvan <depanb187@gmail.com>
2023-03-23 14:51:47 +0700

    Tambah identitas

--- TugasKelompok/src/Main.java
+++ TugasKelompok/src/Main.java
@@ -1,19 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
- *
- * @author ASUS
+ * 1. Adib Sulthon Muammal (222111840)
+ * 2. Adinda Ayu Pramesthi (222111843)
+ * 3. Aisyah Devyta Maharani (222111869)
+ * 4. Aliefta Zulvansyah Bahyperdana (222111873)
+ * 5. Almira Utami (222111877)
  */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main.main()");
 //        adhakldaldb
-//    adkadb dkad adkavd
+//adhadahlDA
 //        cek
-//  test
+//  coba
 //cek cek
 //cek adib yoi bro
     }
