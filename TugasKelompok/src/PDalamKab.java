230a86a53078a59d9dfd6488182a6468290e3d58
adinnn123 <128767365+adinnn123@users.noreply.github.com>
2023-03-24 10:43:08 +0700

    commit adin

---
+++ TugasKelompok/src/PDalamKab.java
@@ -0,0 +1,11 @@
+public class PDalamKab {
+    private ArrayList<Perusahaan> lisPerusahaan = new ArrayList<>();
+
+    public PDalamKab(ArrayList<Perusahaan> lisPerusahaan) {
+        this.lisPerusahaan = lisPerusahaan;
+    }
+
+    public int getNoUrut(){
+        return 0;
+    }
+}
